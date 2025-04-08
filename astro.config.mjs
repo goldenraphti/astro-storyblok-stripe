a78bceb9011c7291c1d541c3f9c70c9d88e045d6
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-04-08 14:54:24 +0200

    integrate storyblok basic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { storyblok } from "@storyblok/astro";
+import { loadEnv } from "vite";
+import netlify from "@astrojs/netlify/functions";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+        blogPost: "storyblok/BlogPost",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+      },
+    }),
+  ],
+});
