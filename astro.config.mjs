be44f13cb1a0d64f8acddaff3aaa73b96f7a4a7a
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-04-08 15:24:32 +0200

    add more block specs & init integrate netlify for visual editing

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,12 +9,19 @@ const env = loadEnv("", process.cwd(), "STORYBLOK");
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: netlify(),
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
+      bridge: true,
       components: {
         // Add your components here
         blogPost: "storyblok/BlogPost",
+        blogPostList: "storyblok/BlogPostList",
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
       },
       apiOptions: {
         // Choose your Storyblok space region
